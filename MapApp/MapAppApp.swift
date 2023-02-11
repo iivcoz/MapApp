b717da3f4ee6f48661bad93611b38a10734277ba
Anas <n7ty1422@gmail.com>
2023-02-11 11:33:32 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Anas on 20/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
